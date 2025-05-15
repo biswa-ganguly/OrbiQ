d89c5eebc98c0adac66d46398728ecd6908c4b28
biswa-ganguly <biswaganguly16@gmail.com>
2025-05-15 22:25:59 +0530

    `Refactor DisplayResult component and remove Google Search API route`
    `Update package.json to include axios dependency`

---
+++ app/api/brave-search-api/route.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { error } from "console"
+import { NextResponse } from "next/server"
+
+export async function POST(req){
+  const {searchInput,searchType} = await req.json()
+
+  if(searchInput){
+
+  
+  const result =await axios.get("https://api.search.brave.com/res/v1/web/search?q="+ searchInput + "&count=5",{
+    headers:{
+      "Accept":"application/json",
+      "Accept-Encoding":"gzip",
+      "X-Subscription-Token":process.env.BRAVE_API_KEY
+    }
+  })
+
+  console.log(result.data)
+  return NextResponse.json(result.data)
+}
+else{
+  return NextResponse.json({error:"Please Pass search query"})
+}
+}
